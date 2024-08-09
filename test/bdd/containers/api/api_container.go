a847cdf3e740219d036bf71c1c3cbe0ce2c4ed45
GabiHert <hertergabriel4@gmail.com>
2024-08-08 22:25:09 -0300

    wip: api setup

--- test/bdd/containers/api/api_container.go
+++ test/bdd/containers/api/api_container.go
@@ -2,94 +2,162 @@ package api_container
 
 import (
 	"bytes"
-	"context"
+	"encoding/json"
 	"fmt"
-	tc "github.com/testcontainers/testcontainers-go/modules/compose"
 	"io"
 	"net/http"
+	"strings"
 )
 
-var compose tc.ComposeStack
-
-func Connect() error {
-	c, err := tc.NewDockerCompose("docker-compose.yml")
-	if err != nil {
-		return err
-	}
-	compose = c
-	return nil
-
-}
-func Disconnect() error {
-	err := compose.Down(context.Background())
-	if err != nil {
-		return err
-	}
-	return nil
+func escapeJSONString(input string) string {
+	return strings.ReplaceAll(input, `"`, `\"`)
 }
 
-func MockPayloadValidationsApiExecuteValidation(response string) {
+type Request struct {
+	Secure         bool   `json:"secure"`
+	KeepAlive      bool   `json:"keepAlive"`
+	Method         string `json:"method"`
+	Path           string `json:"path"`
+	PathParameters []struct {
+		Name   string   `json:"name"`
+		Values []string `json:"values"`
+	} `json:"pathParameters"`
+	QueryStringParameters []struct {
+		Name   string   `json:"name"`
+		Values []string `json:"values"`
+	} `json:"queryStringParameters"`
+	Body struct {
+		Not         bool   `json:"not"`
+		Type        string `json:"type"`
+		Base64Bytes string `json:"base64Bytes"`
+		ContentType string `json:"contentType"`
+	} `json:"body"`
+	Headers []struct {
+		Name   string   `json:"name"`
+		Values []string `json:"values"`
+	} `json:"headers"`
+	Cookies []struct {
+		Name  string `json:"name"`
+		Value string `json:"value"`
+	} `json:"cookies"`
+	SocketAddress struct {
+		Host   string `json:"host"`
+		Port   int    `json:"port"`
+		Scheme string `json:"scheme"`
+	} `json:"socketAddress"`
+	Protocol string `json:"protocol"`
+}
+
+func Reset() error {
+	_, err := http.NewRequest(http.MethodPut, "http://localhost:1080/mockserver/clear", nil)
+	return err
+}
+
+func GetRequests() (*[]Request, error) {
+	client := &http.Client{}
+	req, err := http.NewRequest("PUT", "http://localhost:1080/mockserver/retrieve", nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("accept", "application/json")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	defer func(Body io.ReadCloser) {
+		_ = Body.Close()
+	}(resp.Body)
+
+	requests := &[]map[string]interface{}{}
+	err = json.NewDecoder(resp.Body).Decode(requests)
+	if err != nil {
+		return nil, err
+	}
+
+	return nil, nil
+}
+
+func MockPayloadValidationsApiExecuteValidation(response string) error {
 	jsonString := fmt.Sprintf(`{
    "httpRequest": {
      "method": "POST",
      "path": "/payload_validations/{payload_validation_name}",
      "pathParameters": {
-       "payload_validation_name": ["[A-Z0-9\\-]+"]
+        	"payload_validation_name":["[A-Z0-9\\-]+"]
      }
-   },
+    },
    "httpResponse": {
      "body": "%s"
    }
- })`, response)
+ }`, escapeJSONString(response))
 
-	mockRequest(jsonString)
+	return mockRequest(jsonString)
 }
 
-func MockPromptRoadMapConfigsApiGetPromptRoadMap(promptRoadMapConfigName string, promptRoadMapStep int, response string) {
+func MockPromptRoadMapConfigsApiGetPromptRoadMap(promptRoadMapConfigName string, promptRoadMapStep int, response string) error {
+	jsonString := fmt.Sprintf(`{
+  "httpRequest": {
+    "method": "GET",
+    "path": "/prompt_road_map_configs/{prompt_road_map_config_name}/prompt_road_maps/{step}",
+    "pathParameters": {
+      "prompt_road_map_config_name": ["%s"],
+      "step": ["%d"]
+    } },
+  "httpResponse": {
+    "body": "%s"
+  }
+}`, promptRoadMapConfigName, promptRoadMapStep, escapeJSONString(response))
+
+	return mockRequest(jsonString)
+}
+
+func MockPromptRoadMapConfigsStatus(status int) error {
 	jsonString := fmt.Sprintf(`  {
     "httpRequest": {
       "method": "GET",
       "path": "/prompt_road_map_configs/{prompt_road_map_config_name}/prompt_road_maps/{step}",
       "pathParameters": {
-        "prompt_road_map_config_name": "%s",
-		"step": %d
-      }
+        	"prompt_road_map_config_name":["[A-Z0-9\\-]+"],
+			"step":["[0-9]+"]
+		}
     },
     "httpResponse": {
-      "body": "%s"
+      "statusCode":%d
     }
-  })`, promptRoadMapConfigName, promptRoadMapStep, response)
+  }`, status)
 
-	mockRequest(jsonString)
+	return mockRequest(jsonString)
 }
 
-func MockPromptRoadMapConfigExecutionsApiUpdateStepInExecution(promptRoadMapConfigExecutionId string, stepInExecution int, response string) {
+func MockPromptRoadMapConfigExecutionsApiUpdateStepInExecution(promptRoadMapConfigExecutionId string, stepInExecution int, response string) error {
 	jsonString := fmt.Sprintf(`  {
     "httpRequest": {
       "method": "GET",
       "path": "/prompt_road_map_config_executions/{prompt_road_map_config_execution_id}"/step_in_execution",
       "pathParameters": {
-        "prompt_road_map_config_execution_id": "%s",
-      },
+        	"prompt_road_map_config_execution_id":["%s"],
+		},
       "body": {
         "type": "JSON",
         "step_in_execution": %d
       }
     },
     "httpResponse": {
-      "body": "%s"
+      "body":%s
     }
-  })`, promptRoadMapConfigExecutionId, stepInExecution, response)
+  }`, promptRoadMapConfigExecutionId, stepInExecution, escapeJSONString(response))
 
-	mockRequest(jsonString)
+	return mockRequest(jsonString)
 }
 
-func mockRequest(jsonString string) {
+func mockRequest(jsonString string) error {
 
-	req, err := http.NewRequest("POST", "https://localhost:1080/mockserver/expectation", bytes.NewBuffer([]byte(jsonString)))
+	req, err := http.NewRequest("PUT", "http://localhost:1080/mockserver/expectation", bytes.NewBuffer([]byte(jsonString)))
 	if err != nil {
 		fmt.Println("Error creating request:", err)
-		return
+		return err
 	}
 
 	req.Header.Set("Content-Type", "application/json")
@@ -98,7 +166,7 @@ func mockRequest(jsonString string) {
 	resp, err := client.Do(req)
 	if err != nil {
 		fmt.Println("Error making request:", err)
-		return
+		return err
 	}
 	defer func(Body io.ReadCloser) {
 		_ = Body.Close()
@@ -107,8 +175,8 @@ func mockRequest(jsonString string) {
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		fmt.Println("Error reading response body:", err)
-		return
+		return err
 	}
 	fmt.Println(string(body))
-
+	return nil
 }
--- test/bdd/steps/step_definitions_test.go
+++ test/bdd/steps/step_definitions_test.go
@@ -8,6 +8,7 @@ import (
 	"github.com/trend-me/ai-prompt-builder/internal/config/injector"
 	"github.com/trend-me/ai-prompt-builder/internal/config/properties"
 	"github.com/trend-me/ai-prompt-builder/test/bdd/containers"
+	api_container "github.com/trend-me/ai-prompt-builder/test/bdd/containers/api"
 	rabbitmq_container "github.com/trend-me/ai-prompt-builder/test/bdd/containers/rabbitmq"
 	"testing"
 	"time"
@@ -15,6 +16,7 @@ import (
 import "github.com/cucumber/godog"
 
 var (
+	t               *testing.T
 	consumedMessage string
 	consumer        func(ctx context.Context) (chan error, error)
 )
@@ -35,7 +37,6 @@ func setup(t *testing.T) {
 		}
 		fmt.Println(err.Error())
 	}
-
 	if err != nil {
 		t.Fatal(err.Error())
 	}
@@ -57,7 +58,8 @@ func down(t *testing.T) {
 	}
 }
 
-func TestFeatures(t *testing.T) {
+func TestFeatures(t_ *testing.T) {
+	t = t_
 	setup(t)
 	t.Cleanup(func() {
 		defer down(t)
@@ -84,7 +86,7 @@ func aMessageWithTheFollowingDataIsSentToAipromptbuilderQueue(queue string, arg1
 	return rabbitmq_container.PostMessageToQueue(queue, []byte(arg1.Content))
 }
 
-func aMessageWithTheFollowingDataShouldBeSentToAipromptbuilderQueue(t *testing.T, queue string, arg1 *godog.DocString) error {
+func aMessageWithTheFollowingDataShouldBeSentToAipromptbuilderQueue(queue string, arg1 *godog.DocString) error {
 	content, _, err := rabbitmq_container.ConsumeMessageFromQueue(queue)
 	if err != nil {
 		return err
@@ -94,7 +96,7 @@ func aMessageWithTheFollowingDataShouldBeSentToAipromptbuilderQueue(t *testing.T
 	return nil
 }
 
-func noMessageShouldBeSentToTheAirequesterQueue(t *testing.T, queue string) error {
+func noMessageShouldBeSentToTheAirequesterQueue(queue string) error {
 	content, _, err := rabbitmq_container.ConsumeMessageFromQueue(queue)
 	if err != nil {
 		return err
@@ -157,16 +159,18 @@ func theMetadataShouldNotBeSentToTheValidationAPI() error {
 	return nil
 }
 
-func thePromptRoadMapAPIReturnsAnStatusCode() error {
-	return nil
+func thePromptRoadMapAPIReturnsAnStatusCode500() error {
+	return api_container.MockPromptRoadMapConfigsStatus(500)
 }
 
-func thePromptRoadMapAPIReturnsTheFollowingPromptRoadMap(arg1 *godog.DocString) error {
-	return nil
+func thePromptRoadMapAPIReturnsTheFollowingPromptRoadMap(step int, name string, arg1 *godog.DocString) error {
+	return api_container.MockPromptRoadMapConfigsApiGetPromptRoadMap(name, step, arg1.Content)
 }
 
 func thePrompt_road_mapIsFetchedFromThePromptroadmapapiUsingThePrompt_road_map_config_name() error {
-	return nil
+	_, err := api_container.GetRequests()
+
+	return err
 }
 
 func thePrompt_road_map_config_executionIsUpdatedWithTheCurrentStepOfThePrompt_road_map() error {
@@ -174,10 +178,14 @@ func thePrompt_road_map_config_executionIsUpdatedWithTheCurrentStepOfThePrompt_r
 }
 
 func theValidationAPIReturnsTheFolowingValidationResult(arg1 *godog.DocString) error {
-	return nil
+	return api_container.MockPayloadValidationsApiExecuteValidation(arg1.Content)
 }
 
 func InitializeScenario(ctx *godog.ScenarioContext) {
+	ctx.Before(func(ctx context.Context, sc *godog.Scenario) (context.Context, error) {
+		err := api_container.Reset()
+		return ctx, err
+	})
 	ctx.Step(`^a message with the following data is sent to \'(.*)\' queue:$`, aMessageWithTheFollowingDataIsSentToAipromptbuilderQueue)
 	ctx.Step(`^a message with the following data should be sent to \'(.*)\' queue:$`, aMessageWithTheFollowingDataShouldBeSentToAipromptbuilderQueue)
 	ctx.Step(`^no message should be sent to the \'(.*)\' queue$`, noMessageShouldBeSentToTheAirequesterQueue)
@@ -188,9 +196,9 @@ func InitializeScenario(ctx *godog.ScenarioContext) {
 	ctx.Step(`^the message is consumed by the ai-prompt-builder consumer$`, theMessageIsConsumedByTheAipromptbuilderConsumer)
 	ctx.Step(`^the metadata should be sent to the validation API with the metadata_validation_name \'(.*)\'$`, theMetadataShouldBeSentToTheValidationAPIWithTheMetadata_validation_nameTEST_METADATA)
 	ctx.Step(`^the metadata should not be sent to the validation API$`, theMetadataShouldNotBeSentToTheValidationAPI)
-	ctx.Step(`^the prompt road map API returns an statusCode 500$`, thePromptRoadMapAPIReturnsAnStatusCode)
-	ctx.Step(`^the prompt road map API returns the following prompt road map:$`, thePromptRoadMapAPIReturnsTheFollowingPromptRoadMap)
+	ctx.Step(`^the prompt road map API returns an statusCode 500$`, thePromptRoadMapAPIReturnsAnStatusCode500)
+	ctx.Step(`^the prompt road map API returns the following prompt road map for step \'(\d+)\' and prompt_road_map_config_name \'(.*)\':$`, thePromptRoadMapAPIReturnsTheFollowingPromptRoadMap)
 	ctx.Step(`^the prompt_road_map is fetched from the prompt-road-map-api using the prompt_road_map_config_name \'(.*)\'$`, thePrompt_road_mapIsFetchedFromThePromptroadmapapiUsingThePrompt_road_map_config_name)
-	ctx.Step(`^the prompt_road_map_config_execution step_in_execution is updated to '(\d+)'$`, thePrompt_road_map_config_executionIsUpdatedWithTheCurrentStepOfThePrompt_road_map)
+	ctx.Step(`^the prompt_road_map_config_execution step_in_execution is updated to \'(\d+)\'$`, thePrompt_road_map_config_executionIsUpdatedWithTheCurrentStepOfThePrompt_road_map)
 	ctx.Step(`^the validation API returns the following validation result:$`, theValidationAPIReturnsTheFolowingValidationResult)
 }
